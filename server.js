0594e0f27a2046ab255548017853fc42db5d355e
flavioprado <flavioprado1@gmail.com>
2020-09-25 11:02:30 -0300

    'alt'

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 3000
 const app = express()
- 
+
 app.use(express.static(path.join(__dirname, 'dist', 'app-kotacho')))
- 
+
 app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname, 'dist', 'app-kotacho', 'index.html'))
 })
+app.get('*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'dist', 'app-kotacho', 'index.html'))
+});
 
-console.log('listen on port '+port)
- 
+console.log('listen on port ' + port)
+
 app.listen(port)
