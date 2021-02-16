89a0ab65c0a524eede76dc3610d01a3d0b147f87
flavioprado <flavioprado1@gmail.com>
2021-02-16 01:47:37 -0300

    alt

--- src/environments/dynamic-values.ts
+++ src/environments/dynamic-values.ts
@@ -1,6 +1,6 @@
 // AUTO GENERATED VALUES => Nao edite, pois os dados serao sobrescritos durante o build!
 export const dynamicValues = {
-  version: '1.0.0',
+  version: '1.0.5',
   angularVersion: '~10.0.6',
   primeNgVersion: 'undefined',
   saneNgVersion: 'undefined',
